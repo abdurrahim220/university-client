1c67b4c075a07e9c09054c362f8f854e7d380c57
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-12-22 08:34:18 +0600

    redux presis added

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.tsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { persistor, store } from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
